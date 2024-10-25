3bcdd309c5d55aa35cbfd2d16345185764470010
krystal24gu <xunjieg@unimelb.student.edu.au>
2024-10-25 13:03:15 +1100

    UI design

---
+++ app/src/main/java/com/example/uiappliction/activity/WelcomeActivity.java
@@ -0,0 +1,32 @@
+package com.example.uiappliction.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.uiappliction.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        //透明状态栏
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
+    }
+
+    public void jump(View view) {
+        startActivity(new Intent(WelcomeActivity.this,PageJumpActivity.class));
+    }
+}
