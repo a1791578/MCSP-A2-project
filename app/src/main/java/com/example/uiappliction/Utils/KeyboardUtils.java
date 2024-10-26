15cafc334533c40bdb52f33ef001d3d65cf42c32
chenyuan0219 <1024240209@qq.com>
2024-10-26 17:15:26 +1100

    add login, signup activity

---
+++ app/src/main/java/com/example/uiappliction/Utils/KeyboardUtils.java
@@ -0,0 +1,24 @@
+package com.example.uiappliction.Utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.inputmethod.InputMethodManager;
+
+public class KeyboardUtils {
+    /**
+     * 隐藏软键盘
+     *
+     * @param context 上下文环境
+     * @description
+     * @Author Anduin9527
+     * @date 2022/10/9 19:26
+     * @commit
+     */
+    public static void hideKeyboard(Activity context) {
+        InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (imm != null) {
+            imm.hideSoftInputFromWindow(context.getWindow().getDecorView().getWindowToken(), 0);
+        }
+
+    }
+}
